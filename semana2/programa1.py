b48c4d6b01725fa64b11699ca361acdbd724f015
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-01-23 16:55:06 +0000

    CREATED programa1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,10 @@
+"""
+    Programa 1
+    Nombre: Esquivel Cruz José David
+    Fecha: 23/01/2023
+    Descripcion: Comentarios multilinea - Comentarios de una linea - Concatenacion de cadenas - Cadena de 
+    caracteres
+"""
+variable1 = "Hola "  # Variable que almacena una cadena de caracteres 
+variable2 = "Python"  # Variable que almacena una cadena de caracteres
+print(variable1 + variable2)  # Operacion que concatena 2 cadenas 
