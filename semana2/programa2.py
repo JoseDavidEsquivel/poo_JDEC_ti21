fc63324834e4fe7e17f9017fd6fc2f8dc9c9baff
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-01-24 19:43:20 +0000

    UPDATED

--- semana2/programa2.py
+++ semana2/programa2.py
@@ -1,12 +1,12 @@
 """
-    Programa 1
+    Programa 2
     Nombre: Esquivel Cruz José David
     Fecha: 23/01/2023
     Descripcion: Comentarios multilinea - Comentarios de una linea - Concatenacion de cadenas - Cadena de 
     caracteres
 """
 variable1 = "Hola "  # Variable tipo str 
-variable2 = '619'  # Variable tipo int
+variable2 = '642'  # Variable tipo int
 print(variable1 + variable2)  # Operacion que concatena 2 cadenas 
 
 # No se podra concatenar valores que no sean str (Ejemplo: str + int)
