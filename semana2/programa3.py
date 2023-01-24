02e96f31465efc311c614c986460b77b33b024b3
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-01-24 20:20:23 +0000

    UPDATED | programa3.py

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -6,8 +6,18 @@
 """
 
 numero1 = 10
-numero2 = 10
+numero2 = 5
 
 print(numero1 + numero2) # Este print muestra la operacion que se hace entre ()
 
-print("{} + {} = {}" .format(numero1,numero2,numero1+numero2))
+print("{} + {} = {}" .format(numero1,numero2,numero1+numero2)) # 10 + 5 = 15
+
+print("{} = {} * {}" .format(numero1*numero2,numero2,numero1)) # 50 = 5 * 10
+
+print("El resultado de dividir {} / {} es {}" .format(numero1,numero2,numero1/numero2)) # El resultado de dividir 10 / 5 es 2
+
+"""
+  - En el print con .format se puede añadir operaciones matematicas 
+  - {} podemos añadir .format(numero1+numero2)
+  - mas aparte se puede agregar texto en print("Hola {}") 
+"""
