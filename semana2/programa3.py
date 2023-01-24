cf408411cefd896cc312d21eb2ca63152657dc7c
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-01-24 20:13:49 +0000

    CREATED | programa3.py

---
+++ semana2/programa3.py
@@ -0,0 +1,13 @@
+"""
+    Programa3
+    Nombre: Esquivel Cruz José David
+    Fecha: 24/01/2023
+    Descripcion: print y operaciones aritmeticas
+"""
+
+numero1 = 10
+numero2 = 10
+
+print(numero1 + numero2) # Este print muestra la operacion que se hace entre ()
+
+print("{} + {} = {}" .format(numero1,numero2,numero1+numero2))
