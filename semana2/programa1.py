3c215712085274521470eedf9a4ebb613a6ddd0c
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-01-30 16:51:44 +0000

    CREATED | programa5.py

--- semana2/programa1.py
+++ semana2/programa1.py
@@ -9,4 +9,13 @@ variable1 = "Hola "  # Variable tipo str
 variable2 = '619'  # Variable tipo int
 print(variable1 + variable2)  # Operacion que concatena 2 cadenas 
 
+
+spam = "7"
+
+spam = spam + "0"
+
+eggs = int(spam) + 3
+
+print(float(eggs))
+
 # No se podra concatenar valores que no sean str (Ejemplo: str + int)
---
+++ semana3/programa5.py
@@ -0,0 +1,24 @@
+"""
+    Programa5
+    Nombre: Esquivel Cruz José David
+    Fecha: 30/01/2023
+    Descripcion: Casting
+"""
+
+"""
+numero0 = input("Numero 1 : ") # Esta linea pide una entrada de cualquier tipo (generalmente a str)
+"""
+numero1 = int(input("Numero 1 : ")) # Esta linea pide una entrada y la especifica como tipo int "int(input())"
+numero2 = 10 # Valor fijo de tipo int
+
+suma = numero1+numero2 # Realiza el proceso de las operaciones indicadas
+resta = numero1-numero2
+mult = numero1*numero2
+division = numero1/numero2
+potencia = numero1**numero2
+
+print(suma) # Imprime el resultado de las operaciones correspondientes
+print(resta)
+print(mult)
+print(division)
+print(potencia)
