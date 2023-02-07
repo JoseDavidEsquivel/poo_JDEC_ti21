ae97f62e554102d3e5b58f92f3352b50ba24a3b5
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-02-07 20:57:29 +0000

    UPDATED | programa10.py | Aumento de contenido

--- semana4/programa10.py
+++ semana4/programa10.py
@@ -48,5 +48,31 @@ if n1<=n2:
         print(n2)
 else:
     print(n1)
+    
+# Forma 6
+if n1>n2:  
+    print(n1)
+elif n2>n1: 
+    print(n2)
+elif n1==n2: 
+    print(None)
+
+# Forma 7
+if n1>=n2:
+    if n1==n2:
+        print(None)
+    else:
+        print(n1)
+else:
+    print(n2)
+
+# Forma 8
+if n1>n2:
+    print(n1)
+elif:
+    print()
+    
+    
+
 
     
