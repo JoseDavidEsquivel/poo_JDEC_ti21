309e51895df0eb4a91d18edab1d917d1babc03b6
JoseDavidEsquiv <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-02-17 02:41:42 +0000

    UPDATED | varios programas | complementación de comentarios

--- semana4/programa11.py
+++ semana4/programa11.py
@@ -5,13 +5,13 @@
     Descripcion: definicion de funciones
 """
 
-def mayor(numero1,numero2):
-    result = None
-    if numero1 > numero2:
-        result = numero1
-    elif numero2 > numero1:
-        result = numero2
-    return result
+def mayor(numero1,numero2): # Definicion de la funcion llamada "mayor"
+    result = None # La asignacion de valor "None" a la variable "result"
+    if numero1 > numero2: # if con la condicion de "si numero1 es mayor a numero2"
+        result = numero1 # asignacion de la variable "numero1" como "result"
+    elif numero2 > numero1: # elif con la condicion de "si numero2 es mayor a numero1"
+        result = numero2 # asignacion de la variable "numero2" como "result"
+    return result # funcion que permite que retorne la variable result
 
 print(mayor(2,1)) #2
 print(mayor(1,2)) #2
--- semana4/programa12.py
+++ semana4/programa12.py
@@ -5,13 +5,15 @@
     Descripcion: definicion de funciones (con typing)(solo disponible para versiones posteriores a 3.9 de python)
 """
 
-def mayor(numero1:int,numero2:int) ->int | None:
+def mayor(numero1:int,numero2:int) ->int | None: # Definicion de la funcion llamada "mayor" con la diferencia de que aqui se le esta dando el tipeo de variable entera (int)
     result = None
-    if numero1 > numero2:
-        result = numero1
-    elif numero2 > numero1:
-        result = numero2
-    return result
+    if numero1 > numero2: # if con la condicion de "si numero1 es mayor a numero2"
+        result = numero1 # asignacion de la variable "numero1" como "result"
+    elif numero2 > numero1: # elif con la condicion de "si numero2 es mayor a numero1"
+        result = numero2 # asignacion de la variable "numero2" como "result"
+    return result # funcion que permite que retorne la variable result
+
+       
 
 print(mayor(2,1)) #2
 print(mayor(1,2)) #2
